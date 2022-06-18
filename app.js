b1bba98019161e9fbe7c3bf90382efc32b8ce077
hainguyen <hainv@watermelon.vn>
2022-06-18 12:32:34 +0700

    add

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require("body-parser");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger('dev'));
 
--- routes/auth.js
+++ routes/auth.js
@@ -36,50 +36,61 @@ router.post('/login', function(req, res, next) {
 });
 
 router.post('/register', function(req, res, next) {
-    console.log('start register account')
-    let email = req.body.email;
-    let password = req.body.password;
-    let fullname = req.body.fullname;
-    console.log(req.body)
-    let checkRegister = true;
-    let message = "success"
-    if(email.trim().length == 0 || password.trim().length == 0 || fullname.trim().length == 0){
-        message = 'all data not null'
-        res.send({result: false, message: message})
-    }
-    fs.readFile(userPath, 'utf8', function (err, data) {
-        console.log('start read file data')
-        if (!err) {
-            console.log('start read file data case')
-            users = JSON.parse(data);
-            user = users.filter((item) => {
-                return item.email == email
-            })
-            if(user.length > 0){
-                console.log('email already exists')
-                checkRegister = false
-                message = "email already exists"
-            }else{
-                console.log('check email success')
-                console.log('start insert data to json file')
-                user = {
-                    id: Math.random(),
-                    email: email,
-                    password: password,
-                    fullname: fullname
-                }
-                fs.writeFileSync(userPath, data+JSON.stringify(user), 'utf8');
-                message = "register success"
-                console.log('end insert data to json file')
-            }
-        } else {
-            console.error(err);
-            message = error
-            res.send({result: false, message: message})
+    try{
+        console.log('start register account')
+        let email = req.body.email;
+        let password = req.body.password;
+        let fullname = req.body.fullname;        
+        let checkRegister = true;
+        let message = "success"
+
+        if(!email || !password || !fullname || email.trim().length == 0 || password.trim().length == 0 || fullname.trim().length == 0){
+            message = 'all data not null'
+            return res.send({result: false, message: message})
         }
-        res.send({result: checkUser, message: message})
-    })
-    console.log('end register account')
+        fs.readFile(userPath, 'utf8', function (err, data) {
+            if (!err) {
+                users = JSON.parse(data);
+                
+                user = users.filter((item) => {
+                    return item.email == email
+                })
+                if(user.length > 0){
+                    console.log('email already exists')
+                    checkRegister = false
+                    message = "email already exists"
+                }else{
+                    user = {
+                        id: Math.random(),
+                        email: email,
+                        password: password,
+                        fullname: fullname
+                    }
+                    try{
+                        appendData = new Array(...users, user)
+                        console.log('appendData')
+                        console.log(appendData)
+                        fs.writeFileSync(userPath, JSON.stringify(appendData), 'utf8');
+                        message = "register success"
+                        console.log('end insert data to json file')
+                        return res.send({result: checkRegister, message: message})
+                    }catch(error){
+                        return res.send({result: false, message: "can not write file"})
+                    }
+                    
+                }
+            } else {
+                console.error(err);
+                message = error
+                return res.send({result: false, message: message})
+            }
+            return res.send({result: checkRegister, message: message})
+        })
+    }catch(e){
+        console.log('end register account')
+        return res.send({result: false, message: "Bad request"})
+    }
+    
 });
 
 module.exports = router;
